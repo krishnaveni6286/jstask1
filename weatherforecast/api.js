e0b668d954bc90b4b62b6f9d132f4e4fb413aac5
krishnaveni6286 <147615070+krishnaveni6286@users.noreply.github.com>
2024-06-09 16:19:39 +0530

    Update api.js

--- weatherforecast/api.js
+++ weatherforecast/api.js
@@ -14,6 +14,7 @@ export const fetchData =function (URL, callback) {
     fetch (`${URL}&appid=${api_key}`)
     .then(res => res.json())
     .then(data =>callback(data))
+     .catch(error => console.error('Error fetching data:', error));
 }
 
 export const url = {
